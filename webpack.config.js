b7c337f9c66031ab93221173a0f61e734fbff9a3
Long Li <lilong@gmail.com>
2024-10-07 00:00:16 +0900

    remove module type

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,33 @@
-import path from 'path';
-import url from 'url';
-import TerserPlugin from "terser-webpack-plugin";
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
-const cwd = url.fileURLToPath (import.meta.url);
-const outputDirectory = path.normalize (path.join (path.dirname (cwd), 'dist'))
-
-export default {
-  entry : './ts/ccxt.ts',
-  output: {
-    path: outputDirectory,
-    filename: 'ccxt.browser.js',
-    library: {
-      type: 'self', // we are targeting the browser (including webworkers)
-      name: 'ccxt',
+module.exports = {
+    entry: './js/ccxt.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'ccxt.browser.js',
+        library: 'ccxt',
+        libraryTarget: 'umd',
+        globalObject: 'this'
     },
-    chunkFormat: 'array-push',
-    chunkLoading: 'jsonp',
-  },
-  cache: {
-    type: 'filesystem',
-  },
-  module: {
-    rules: [{
-      test: /\.ts$/,
-      use: 'ts-loader',
-      exclude: [ /node_modules/ ],
-      sideEffects: false,
-    }],
-  },
-  resolve: {
-    extensions: [ '.ts' ],
-    // this line is needed because we use import xxx.js in ccxt
-    extensionAlias: {
-     '.js': [ '.js', '.ts' ],
+    mode: 'production',
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
-  },
-  mode: 'production',
-  target: 'web',
-  optimization: {
-    minimize: false,
-    minimizer: [new TerserPlugin ({ extractComments: false })],
-    usedExports: true, // these two lines line turns on tree shaking
-    concatenateModules: false,
-  },
-}
+    resolve: {
+        extensions: ['.ts', '.js'],
+        alias: {
+            '@': path.resolve(__dirname, 'ts'),
+        }
+    },
+    optimization: {
+        minimize: false,
+    },
+};
